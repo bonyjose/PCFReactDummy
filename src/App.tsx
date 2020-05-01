6bdd35d894644cb08c5291e2f73d61d908825a0e
Nijomon <nijomon.jose@teranet.ca>
2020-05-01 16:59:46 +0530

    initial commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,97 @@
 import React from 'react';
-import logo from './primereact-logo.png';
+// import logo from './primereact-logo.png';
 import './App.css';
 
-import { Button } from 'primereact/button';
+// import { Button } from 'primereact/button';
 
 import 'primereact/resources/primereact.min.css';
 import 'primereact/resources/themes/nova-light/theme.css';
 import 'primeicons/primeicons.css';
+import {DataTable} from 'primereact/datatable';
+import {Column} from 'primereact/column';
 
 interface Props {}
 interface State {
     count: number;
 }
 
-class App extends React.Component<Props, State> {
+export class App extends React.Component<Props, State> {
+    products: { id: string; name: string;place :string; price: string; }[];
+ 
     
     constructor(props: Props) {
         super(props);
-        this.state = {
-            count: 0
-        };
+        // this.state = {
+        //     count: 0,
+        // };
+        this.products =[
+            {
+            id: "1",
+            name: "nijomon",
+            place : "Kply",
+            price : "30"
+            },
+            {
+                id: "2",
+                name: "nijomon",
+                place : "Kply",
+                price : "30"
+                },
+                {
+                    id: "3",
+                    name: "nijomon",
+                    place : "Kply",
+                    price : "30"
+                    },
+                    {
+                        id: "4",
+                        name: "nijomon",
+                        place : "Kply",
+                        price : "30"
+                        },
+                        {
+                            id: "5",
+                            name: "nijomon",
+                            place : "Kply",
+                            price : "30"
+                            },
+                            {
+                                id: "6",
+                                name: "nijomon",
+                                place : "Kply",
+                                price : "30"
+                                },
+                                {
+                                    id: "7",
+                                    name: "nijomon",
+                                    place : "Kply",
+                                    price : "30"
+                                    }
+        ];
         this.increment = this.increment.bind(this);
-    }
+    };
+
+
     
     increment() {
-        this.setState({
-            count: this.state.count + 1
-        });
+        // this.setState({
+        //     count: this.state.count + 1
+        // });
     }
+
+    // products:[];
     
-    render() {
+   public  render() {
         return (
             <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                </div>
-                <br/>
-                <Button label="PrimeReact" icon="pi pi-check" onClick={this.increment} />
-                <p>Number of Clicks:{this.state.count}</p>
+                <h3>DataTable</h3>
+                <DataTable value={this.products} paginator ={true} rows={2}
+                rowsPerPageOptions ={[5,10,30]}>
+                    <Column field ="id" header ="ID" />
+                    <Column field ="name" header ="Name" />
+                    <Column field ="PRice" header ="Price" />
+                    <Column field ="place" header ="place" />
+                </DataTable>
             </div>
         );
     }
